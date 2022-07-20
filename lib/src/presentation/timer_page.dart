5161df4ad051ba807c1dfde5293499e858f82e0d
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2022-07-20 21:00:20 +0100

    Implement UI

--- lib/src/presentation/timer_page.dart
+++ lib/src/presentation/timer_page.dart
@@ -12,48 +12,46 @@ class TimerPage extends StatelessWidget {
     );
   }
 
-  Center _buildScaffoldBody() {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(Insets.lGutter),
+  Widget _buildScaffoldBody() {
+    return Padding(
+      padding: EdgeInsets.all(Insets.lGutter),
+      child: SingleChildScrollView(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const CurrentTaskTile(),
             const HSpace(size: Insets.m),
             Row(
-              children: const [
-                Expanded(
-                    child: ProgressTile(
-                  color: Color(0xFFFBE0D7),
-                  title: 'In-progress',
-                  currentFigure: 10,
-                  overallFigure: 20,
-                )),
-                VSpace(size: Insets.m),
-                Expanded(
-                    child: ProgressTile(
-                  color: Color(0xFFCBEFED),
-                  title: 'Completed',
-                  currentFigure: 15,
-                  overallFigure: 50,
-                )),
-              ],
-            ),
-            const HSpace(size: Insets.l),
-            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Text(
-                  "Daily Tasks",
+                  "Daily Insights",
                   style: TextStyle(
                     fontSize: 16.0,
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                TextButton(onPressed: () {}, child: const Text('See all'))
+                TextButton(onPressed: () {}, child: const Text('See more')),
               ],
             ),
+            const DailyInsightsWidget(),
+            const HSpace(size: Insets.xl),
+            const Text(
+              "Tasks",
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            ListView.separated(
+              itemCount: 4,
+              separatorBuilder: (context, index) => const Divider(),
+              shrinkWrap: true,
+              itemBuilder: ((context, index) {
+                return const TaskListTile();
+              }),
+            )
           ],
         ),
       ),
@@ -76,6 +74,197 @@ class TimerPage extends StatelessWidget {
           icon: const Icon(Icons.settings),
         )
       ],
+    );
+  }
+}
+
+class DailyInsightsWidget extends StatelessWidget {
+  const DailyInsightsWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: ListView(
+        physics: const BouncingScrollPhysics(),
+        clipBehavior: Clip.none,
+        scrollDirection: Axis.horizontal,
+        children: [
+          StyledCard(
+            color: const Color(0xFFFBE0D7),
+            paddingInsets: Insets.m,
+            child: SizedBox(
+              width: 100,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.pending_actions,
+                    color: Color(0xFFEF835F),
+                  ),
+                  HSpace(size: Insets.sm),
+                  Text(
+                    'Ongoing',
+                    style: TextStyle(
+                      color: Color(0xFFEF835F),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  HSpace(size: Insets.m),
+                  Text(
+                    '5/10',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 21,
+                    ),
+                  ),
+                  HSpace(size: Insets.m),
+                  LinearProgressIndicator(value: 0.5),
+                  HSpace(size: Insets.m),
+                  Text(
+                    'Est: 4hr',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const VSpace(size: Insets.m),
+          StyledCard(
+            color: const Color(0xFFCBEFED),
+            paddingInsets: Insets.m,
+            child: SizedBox(
+              width: 100,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.check_circle,
+                    color: Color(0xFF419A9E),
+                  ),
+                  HSpace(size: Insets.sm),
+                  Text(
+                    'Completed',
+                    style: TextStyle(
+                      color: Color(0xFF419A9E),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  HSpace(size: Insets.m),
+                  Text(
+                    '5/10',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 21,
+                    ),
+                  ),
+                  HSpace(size: Insets.m),
+                  LinearProgressIndicator(value: 0.5),
+                  HSpace(size: Insets.m),
+                  Text(
+                    'Est: 4hr',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const VSpace(size: Insets.m),
+          StyledCard(
+            color: const Color(0xFFFEF1D5),
+            paddingInsets: Insets.m,
+            child: SizedBox(
+              width: 100,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.view_week,
+                    color: Color(0xFFDE9C32),
+                  ),
+                  const HSpace(size: Insets.sm),
+                  const Text(
+                    'Weekly Total',
+                    style: TextStyle(
+                      color: Color(0xFFDE9C32),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const HSpace(size: Insets.m),
+                  const Text(
+                    '5 hr 10m',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 21,
+                    ),
+                  ),
+                  const HSpace(size: Insets.sm),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(Insets.xs),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFFFDDEDF),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_downward,
+                          size: 12,
+                          color: Colors.red,
+                        ),
+                      ),
+                      const VSpace(size: Insets.sm),
+                      const Text(
+                        '-7.31%',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                  const HSpace(size: Insets.sm),
+                  const Text(
+                    '20/07 - ',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TaskListTile extends StatelessWidget {
+  const TaskListTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: Checkbox(
+        activeColor: Colors.green,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        value: true,
+        onChanged: (value) {},
+      ),
+      title: const Text('Pomodoro Mobile App'),
+      trailing: const Text(
+        '0/4',
+        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16.0),
+      ),
     );
   }
 }
@@ -221,10 +410,12 @@ class StyledCard extends StatelessWidget {
     super.key,
     required this.child,
     this.color = const Color(0xFFE1E5FF),
+    this.paddingInsets = Insets.l,
   });
 
   final Widget child;
   final Color color;
+  final double paddingInsets;
 
   @override
   Widget build(BuildContext context) {
@@ -234,7 +425,7 @@ class StyledCard extends StatelessWidget {
         color: color,
       ),
       child: Padding(
-        padding: const EdgeInsets.all(Insets.l),
+        padding: EdgeInsets.all(paddingInsets),
         child: child,
       ),
     );
