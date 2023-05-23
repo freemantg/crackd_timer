814cda95c1fd67c4d55ab5b0a1b44e88e6a20124
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2023-05-23 17:56:19 +0100

    Refactored the initialisation of SembastDatabase

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,6 @@ import 'src/app_widget.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await di.init();
-  // await di.getIt<SembastDatabase>().init();
-
   runApp(
     const ProviderScope(child: AppWidget()),
   );
--- lib/src/infrastructure/core/sembast_database.dart
+++ lib/src/infrastructure/core/sembast_database.dart
@@ -1,26 +1,22 @@
 import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
 
-class SembastDatabase {
-  late Database _instance;
-  
-  Database get instance => _instance;
-
-  bool _hasBeenInitialized = false;
+class SembastDatabase extends StateNotifier<Database?> {
+  SembastDatabase() : super(null) {
+    init;
+  }
 
   Future<void> init() async {
-    if (_hasBeenInitialized) return;
-    _hasBeenInitialized = true;
-
     //Get a platform-specific directory where persistent app data can be stored
     final dbDirectory = await getApplicationDocumentsDirectory();
     dbDirectory.create(recursive: true);
     //path with the form: /platform-specific-directory/db.sembast
     final dbPath = join(dbDirectory.path, 'db.sembast');
-    _instance = await databaseFactoryIo.openDatabase(dbPath);
+    state = await databaseFactoryIo.openDatabase(dbPath);
   }
 }
