f8c1797daeaf56eb7e696c3cda0e2d7531ac97fa
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2023-05-24 13:45:40 +0100

    Refactored color_selector

--- lib/src/presentation/add_task/widgets/color_selector.dart
+++ lib/src/presentation/add_task/widgets/color_selector.dart
@@ -27,68 +27,49 @@ class ColorSelector extends StatelessWidget {
           const Center(child: Icon(Icons.keyboard_arrow_down)),
           Text('Color', style: TextStyles.h2),
           const HSpace(size: Insets.m),
-          const _ColorListViewBuilder(),
+          _buildColorListView(),
         ],
       ),
     );
   }
 }
 
-class _ColorListViewBuilder extends StatelessWidget {
-  const _ColorListViewBuilder({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 48,
-      child: ListView.builder(
-        shrinkWrap: true,
-        clipBehavior: Clip.none,
-        scrollDirection: Axis.horizontal,
-        itemCount: colorsList.length,
-        itemBuilder: ((context, index) {
-          final color = colorsList[index];
-          return _ColorTile(color: color);
-        }),
-      ),
-    );
-  }
+Widget _buildColorListView() {
+  return SizedBox(
+    height: 48,
+    child: ListView.builder(
+      shrinkWrap: true,
+      clipBehavior: Clip.none,
+      scrollDirection: Axis.horizontal,
+      itemCount: colorsList.length,
+      itemBuilder: ((context, index) {
+        final color = colorsList[index];
+        return _buildColorTile(color);
+      }),
+    ),
+  );
 }
 
-class _ColorTile extends StatelessWidget {
-  const _ColorTile({
-    Key? key,
-    required this.color,
-  }) : super(key: key);
+Widget _buildColorTile(Color color) {
+  return BlocBuilder<TaskFormBloc, TaskFormState>(
+    builder: (context, state) {
+      final isSelected = state.task.taskColor == color;
 
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<TaskFormBloc, TaskFormState>(
-      builder: (context, state) {
-        final isSelected = state.task.taskColor == color;
-
-        return GestureDetector(
-          onTap: () => context
-              .read<TaskFormBloc>()
-              .add(TaskFormEvent.colorChanged(color)),
-          child: Container(
-            margin: const EdgeInsets.only(right: Insets.m),
-            height: 48,
-            width: 48,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(Corners.s10),
-              border: isSelected
-                  ? Border.all(color: Colors.white, width: 4.0)
-                  : null,
-            ),
+      return GestureDetector(
+        onTap: () =>
+            context.read<TaskFormBloc>().add(TaskFormEvent.colorChanged(color)),
+        child: Container(
+          margin: const EdgeInsets.only(right: Insets.m),
+          height: 48,
+          width: 48,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(Corners.s10),
+            border:
+                isSelected ? Border.all(color: Colors.white, width: 4.0) : null,
           ),
-        );
-      },
-    );
-  }
+        ),
+      );
+    },
+  );
 }
