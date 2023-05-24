99c30828d26af33e7ca616cd7184c87ca505fab0
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2023-05-25 00:25:30 +0100

    AlarmCubit Unit Tested

--- test/src/application/settings/alarm_cubit_test.dart
+++ test/src/application/settings/alarm_cubit_test.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:crackd_timer/src/application/cubits.dart';
+import 'package:crackd_timer/src/domain/audio/audio_playback_failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:crackd_timer/src/infrastructure/timer/audio_repository.dart';
@@ -21,4 +24,68 @@ void main() {
   test('initial state should be AlarmState.initial', () {
     expect(alarmCubit.state, AlarmState.initial());
   });
+
+  blocTest<AlarmCubit, AlarmState>(
+    'updateAlarmSound should emit updated state with new AlarmSound',
+    build: () => alarmCubit,
+    act: (cubit) => cubit.updateAlarmSound(AlarmSound.bird),
+    expect: () => [AlarmState.initial().copyWith(alarmSound: AlarmSound.bird)],
+  );
+
+  blocTest<AlarmCubit, AlarmState>(
+    'toggleTickingSound should invert tickingSound state',
+    build: () => alarmCubit,
+    act: (cubit) => cubit.toggleTickingSound(),
+    expect: () => [AlarmState.initial().copyWith(tickingSound: false)],
+  );
+
+  blocTest<AlarmCubit, AlarmState>(
+    'playTickSound should handle audio repository playback',
+    build: () {
+      when(() => mockAudioRepository.play(any()))
+          .thenAnswer((_) async => right(unit));
+      return alarmCubit;
+    },
+    act: (cubit) => cubit.playTickSound(),
+    expect: () => [AlarmState.initial().copyWith(failure: null)],
+  );
+
+  blocTest<AlarmCubit, AlarmState>(
+    'playTickSound should handle audio repository playback failure',
+    build: () {
+      when(() => mockAudioRepository.play(any()))
+          .thenAnswer((_) async => left(const AudioPlaybackFailure.unknown()));
+      return alarmCubit;
+    },
+    act: (cubit) => cubit.playTickSound(),
+    expect: () => [
+      AlarmState.initial()
+          .copyWith(failure: const AudioPlaybackFailure.unknown())
+    ],
+  );
+
+  blocTest<AlarmCubit, AlarmState>(
+    'playAlarm should handle audio repository playback',
+    build: () {
+      when(() => mockAudioRepository.play(any()))
+          .thenAnswer((_) async => right(unit));
+      return alarmCubit;
+    },
+    act: (cubit) => cubit.playAlarm(),
+    expect: () => [AlarmState.initial().copyWith(failure: null)],
+  );
+
+  blocTest<AlarmCubit, AlarmState>(
+    'playAlarm should handle audio repository playback failure',
+    build: () {
+      when(() => mockAudioRepository.play(any()))
+          .thenAnswer((_) async => left(const AudioPlaybackFailure.unknown()));
+      return alarmCubit;
+    },
+    act: (cubit) => cubit.playAlarm(),
+    expect: () => [
+      AlarmState.initial()
+          .copyWith(failure: const AudioPlaybackFailure.unknown())
+    ],
+  );
 }
